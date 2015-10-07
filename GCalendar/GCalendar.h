00af2478422973bb8aff36414462a851c46bb266
Jeremy Debate <jezzadebate@mac.com>
2015-10-07 14:37:51 -0400

    Create Xcode project

---
+++ GCalendar/GCalendar.h
@@ -0,0 +1,27 @@
+//
+//  GCalendar.h
+//  GCalendar
+//
+//  Created by Jeremy Debate on 10/7/15.
+//  Copyright © 2015 Giu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FSCalendar.h"
+#import "FSCalendarCell.h"
+#import "FSCalendarConstance.h"
+#import "FSCalendarDynamicHeader.h"
+#import "FSCalendarHeader.h"
+#import "FSCalendarStickyHeader.h"
+#import "FSCalendarHeaderTouchDeliver.h"
+
+
+//! Project version number for GCalendar.
+FOUNDATION_EXPORT double GCalendarVersionNumber;
+
+//! Project version string for GCalendar.
+FOUNDATION_EXPORT const unsigned char GCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GCalendar/PublicHeader.h>
+
+
